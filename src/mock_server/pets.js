1c38ce27dc17120b91f06f46034270f8d7c2d70b
Lulu <yuntaol@student.unimelb.edu.au>
2022-09-10 18:25:24 +1000

    remove all nodemodules file

---
+++ src/mock_server/pets.js
@@ -0,0 +1,36 @@
+[
+    {
+      "id": 0,
+      "adopted": "YES",
+      "category": "CAT",
+      "picture": [
+        "string"
+      ],
+      "nickname": "string",
+      "detail": "string",
+      "adoptionApplicationDTOs": [
+        {
+          "id": 0,
+          "timestamp": "2022-09-10T08:22:58.064Z",
+          "reason": "string",
+          "passport": "string",
+          "applicationStatus": "COMPLETE",
+          "userId": 0,
+          "petId": 0
+        }
+      ],
+      "userAdoptPetDTOS": [
+        {
+          "id": 0,
+          "timeAdopted": "2022-09-10T08:22:58.064Z",
+          "userId": 0,
+          "petId": 0
+        }
+      ],
+      "color": "RED",
+      "sex": "MALE",
+      "age": 0,
+      "character": "string",
+      "immunization": "YES"
+    }
+  ]
