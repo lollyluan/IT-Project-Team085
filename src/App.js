c05c60fdf90314a3d872c4e7603972e815b13536
luanyl <lollyluan999@qq.com>
2022-09-10 16:35:19 +1000

    Router testing

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginPage from './pages/Login/login';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
