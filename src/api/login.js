4afa02a62f7eb09734b17b24d556ed576167391d
Lulu <yuntaol@student.unimelb.edu.au>
2022-09-11 23:07:00 +1000

    login api(not finished

---
+++ src/api/login.js
@@ -0,0 +1,110 @@
+
+
+import pkg from 'jwt-decode';
+const {jwt_decode} = pkg;
+import { BehaviorSubject } from 'rxjs';
+import express from"express"
+
+const BASE_URL = "http://localhost:8080/api/v1"
+//process.env.REACT_APP_BASE_URL
+
+//const currentUserSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('currentUser')));
+//const currentUser=currentUserSubject.asObservable()
+import Window from 'window' 
+const jsdomConfig = { userAgent: 'Custom UA' };
+const window = new Window();
+var app = express();
+app.get('/', function(req, res){
+    res.send(login('yuntaol@student.unimelb.edu.au', '123456'))
+});
+app.listen(3000);
+function login(email, password) {
+    const url = BASE_URL + '/auth/user/login';
+    const requestInit = {
+        method: 'POST',
+        // mode: 'no-cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            'email': email,
+            'password': password
+        })
+    }
+   
+    fetch(url, requestInit)
+    .then(res => {
+        if(res.ok) {
+          
+            return res.json();
+            
+        }
+        else {
+            return Promise.reject();
+        }
+    })
+    .then(user => {
+     
+        // store user details and jwt token in local storage to keep user logged in between page refreshes
+        window.localStorage.setItem('currentUser', JSON.stringify(user));
+        
+        currentUserSubject.next(user);
+
+        return user;
+    })
+    .catch((e) => {
+        console.log('Failed to sign in, try again ' + e);
+        //document.location.href = '/home';
+    });
+}
+
+function signUp(username, password, email, phoneNumber, postcode, address) {
+    const url = BASE_URL + '/auth/user/register';
+    const requestInit = {
+        method: 'POST',
+        // mode: 'no-cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+       
+            'password': password,
+            'email': email,
+            'firstname': firstname,
+            'lastname': lastname
+        })
+    }
+
+    fetch(url, requestInit)
+    .then(res => {
+        console.log(res);
+        if(res.ok) {
+            return res.json();
+        }
+        else {
+            return Promise.reject();
+        }
+    })
+    .then(data => {
+        setCookie('token', data.token, data.expiresIn)
+        document.location.href = '/home';
+    })
+    .catch(() => alert('Failed to sign up, try again'));
+}
+
+function isLoggedIn() {
+    return getCookie('token') !== '';
+}
+
+function getUserId() {
+    if(isLoggedIn()) {
+        
+        const jwt =jwt_decode(getCookie('token'))
+
+        return jwt.userId;
+    } 
+    return undefined;
+}
+
+export {
+    login,
+    signUp,
+    isLoggedIn,
+    getUserId
+}
---
+++ src/api/utils.js
@@ -0,0 +1,35 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+// Set cookie when login
+function setCookie(cname, cvalue, expTime) {
+    const d = new Date();
+    d.setTime(d.getTime() + (expTime * 1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+// Deletes the token of the user
+function deleteUserToken() {
+    setCookie('token', '', 0);
+}
+
+
+export {
+    getCookie,
+    setCookie,
+    deleteUserToken
+}
