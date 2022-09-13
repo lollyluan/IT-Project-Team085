ccff9ae4c6a3f82d69656c168f7b8f63e34909f1
luanyl <lollyluan999@qq.com>
2022-09-13 21:22:23 +1000

    Add search bar

---
+++ src/Components/searchBar.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { BsSearch } from 'react-icons/bs';
+import { BsFilter } from 'react-icons/bs';
+
+function SearchBar() {
+  return (
+    <>
+      <InputGroup className="search-bar" variant="outline-success">
+        <Button variant="outline-success">< BsSearch/></Button>
+        <Button variant="outline-success">< BsFilter/></Button>
+        <Form.Control aria-label="type"/>
+      </InputGroup>
+    </>
+  );
+}
+
+export default SearchBar;
