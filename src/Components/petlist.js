cf089518d0e9749ea07ac0af5972b38c6d9088ad
luanyl <lollyluan999@qq.com>
2022-09-22 13:11:25 +1000

    Add pet router

--- src/Components/petlist.js
+++ src/Components/petlist.js
@@ -1,15 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import img from '../images/cat1.png';
 import Button from "react-bootstrap/Button";
-
+import PetInfo from '../pages/petInformation';
 
 function PetList() {
   return (
-    <Row xs={1} md={5} className="g-4">
+    <Row xs={1} md={5} className="petlist">
       {Array.from({ length: 15 }).map((_, idx) => (
         <Col>
           <Card>
@@ -19,12 +25,17 @@ function PetList() {
               <Card.Text>
                 Age: XX months
               </Card.Text>
-              <Button variant="success">More Information</Button>
+                <Link to="/petInfo">
+                  <Button variant="success">
+                    More Information
+                  </Button>
+                </Link>
             </Card.Body>
           </Card>
         </Col>
-      ))}
+      ))}   
     </Row>
+
   );
 }
 
---
+++ src/router/pets.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+import DisplayPets from '../pages/Petlist/petlist';
+import PetInfo from '../pages/petInformation';
+
+function PetRouter(){
+    return(
+        <BrowserRouter>
+
+            <Routes>
+                <Route path="/petlist" element={<DisplayPets/>}/>                    
+
+                <Route path="/petInfo" element={<PetInfo/>}/>                  
+            </Routes>
+
+        </BrowserRouter>
+    );
+}
+ 
+export default PetRouter;
