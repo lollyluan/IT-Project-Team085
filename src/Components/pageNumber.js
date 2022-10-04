39ecc3d1d6d06e2bcac877c3c2b22290dd9d9597
Lulu <yuntaol@student.unimelb.edu.au>
2022-10-04 19:33:29 +1100

    switch page

--- src/Components/pageNumber.js
+++ src/Components/pageNumber.js
@@ -3,14 +3,14 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
-function PageNumber() {
+function PageNumber(props) {
   return (
     <>
       <ButtonGroup>
-        <Button variant="outline-success">1</Button>{' '}
-        <Button variant="outline-success">2</Button>{' '}
-        <Button variant="outline-success">3</Button>{' '}
-        <Button variant="outline-success">4</Button>
+        <Button value={1} onClick={props.onClick} type="input" variant="outline-success">1</Button>{' '}
+        <Button value={2} onClick={props.onClick} type="input" variant="outline-success">2</Button>{' '}
+        <Button value={3} onClick={props.onClick} type="input" variant="outline-success">3</Button>{' '}
+        <Button value={4} onClic={props.onClick} type="input" variant="outline-success">4</Button>
       </ButtonGroup>
     </>
   );
