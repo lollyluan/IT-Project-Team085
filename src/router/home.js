7f11728f09ed71c68265edac6865adf9e63e9776
Potato-Gravy <min.zhang.1024@gmail.com>
2022-10-07 21:55:25 +1100

    fix server start bug on home.js (import name)

--- src/router/home.js
+++ src/router/home.js
@@ -1,15 +1,15 @@
-import { Login } from "@mui/icons-material";
-import React from "react";
+import { Login } from "@mui/icons-material"
+import React from "react"
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link
-} from "react-router-dom";
-import HomePage from "../pages/HomePage/homepage";
-import LoginPage from "../pages/Login/login";
-import RegisterPage from "../pages/Register/register";
-export default function HomeRouter() {
+} from "react-router-dom"
+import HomePage from "../pages/Homepage/HomePage"
+import LoginPage from "../pages/Login/login"
+import RegisterPage from "../pages/Register/register"
+export default function HomeRouter () {
   return (
     <Router>
       <div>
@@ -30,17 +30,17 @@ export default function HomeRouter() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Routes>
-          <Route exact path="/register" element = {<RegisterPage/>}>
-     
+          <Route exact path="/register" element={<RegisterPage />}>
+
           </Route>
-          <Route exact path="/login" element={<LoginPage/>}>
-         
+          <Route exact path="/login" element={<LoginPage />}>
+
           </Route>
-          <Route exact path="/" element={<HomePage/>}>
-           
+          <Route exact path="/" element={<HomePage />}>
+
           </Route>
         </Routes>
       </div>
     </Router>
-  );
+  )
 }
