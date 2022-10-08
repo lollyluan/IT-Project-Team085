d1c1aec44d9cdba47c07131f4a2f4a9425a6c609
luanyl <lollyluan999@qq.com>
2022-10-08 17:18:31 +1100

    Add component for application pop up

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeRouter from './router/home';
 import PetRouter from './router/pets';
-import { Pets } from '@mui/icons-material';
-import PetList from './Components/petlist';
+
 function App() {
   return (
     <div className="App">
