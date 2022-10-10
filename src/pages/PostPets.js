2a17c5d4de9530b2b7589a91fd3be80194611154
Meilun <yaomeilun1234@gmail.com>
2022-10-10 22:45:31 +1100

    Add post Animation Function finished

--- src/pages/PostPets.js
+++ src/pages/PostPets.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Nav,NavbarBrand } from 'reactstrap';
+import { Nav,NavbarBrand,Button } from 'reactstrap';
 import AdminSideBar from '../Components/AdminSideBar';
 import PostPet from '../Components/post';
+import $ from 'jquery';
+import { Box2Fill } from 'react-bootstrap-icons';
+
+$(document).ready(function () {
+  $('button.addPostTest').on('click',function(){
+        var box = $('form.Post').html();
+        $('div.App > div').append("<form class='post2'>");
+        $('div.App > div form.post2').append(box);
+        console.log('added post');
+    });
+});
 
 function PostPets(){
+
+  
+
     return(
+
+        
 <div>
     <Nav>
         <NavbarBrand href="/">Pets Home</NavbarBrand>
     </Nav>  
     <AdminSideBar/> 
     <PostPet/>
-
+    <Button className="addPostTest">+</Button>
 </div>
 
+        
 
     )
+
 }
+
 
 export default PostPets
