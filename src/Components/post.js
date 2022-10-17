d9471eff77dba17bea8757b663fc64776cf25160
Meilun <yaomeilun1234@gmail.com>
2022-10-17 16:10:56 +1100

    post connect backend

--- src/Components/post.js
+++ src/Components/post.js
@@ -3,46 +3,65 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Form, FormGroup, Input,Button,Row} from 'reactstrap';
 import {postPet} from '../api/pet';
 
-const PostPet =() =>{
+//const PostPet =() =>{
+//const [detail, setDetail] = useState('');
+//const [category, setCategory] = useState('--');
+//const [country, setCountry] = useState('--');
+//const [city, setCity] = useState('--');
+//const [ID, setId] = useState('--- --- ---');
+//const [age, setAge] = useState('');
+//const [nickname,setNickname] = useState('---');
+//const [sex, setSex] = useState('--');
+//const [color, setColor] = useState('--');
+//const [immunization,setImmunization] = useState('--');
 
-  const [Descirption, setDescirption] = useState('');
-  const [Category, setCategory] = useState('CAT');
-  const [Country, setCountry] = useState('Australia');
-  const [City, setCity] = useState('Sydney');
-  const [ID, setId] = useState('');
-  const [Age, setAge] = useState('');
-  const [Name,setName] = useState('');
-  const [Gender, setGender] = useState('MALE');
-  const [Color, setColor] = useState('RED');
-  const [Immunization,setImmunization] = useState('Yes');
-  const [isPending,setIsPending] = useState(false);
+function PostPet(){
+  const[data,setData] = useState({
+        category: "---",
+        nickname: "---",
+        detail: "---",
+        color: "---",
+        sex: "---",
+        age: "---",
+        immunization: "---",
+        city: "---",
+        country: "---"
+  })
 
-
-    //TODO connect backend
-    const handleSubmit = (e)=> {
+    function submit (e){
       e.preventDefault();
-      console.log(`${Category},${Name},${Country},${City},${Age},${Gender},${Color},${Immunization}`);
-      if(isPending == 'true') {
-          
-          postPet(Category,Name,Country,City,Age,Gender,Color,Immunization)
-
-      }else {
-          alert('Still Adding');
-      }
-      
+      postPet(url,{
+        category: data.category,
+        nickname: data.nickname,
+        detail: data.detail,
+        color: data.color,
+        sex: data.sex,
+        age: data.age,
+        immunization: data.immunization,
+        city: data.city,
+        country: data.country
+      })
+      .then(res =>{
+        console.log(res.data)
+      })
   }
 
+  function handle(e){
+    const newdata = {...data}
+    newdata[e.target.value] = e.target.value
+    setData(newdata)
+    console.log(newdata)
+  }
 
-   
     return(
-        <Form className="Post" onSubmit={handleSubmit}>   
+        <Form className="Post" onSubmit={(e) => submit(e)}>   
         <Row className='NoPadding'>
         <Col lg='4'>
         <FormGroup row className="row-cols-lg-auto g-3 align-items-center">
           <div className='s1'>
           <span for="Category" sm='1'>Category:</span>
           </div>
-          <Input  type="select" value={Category} onChange={(e) => setCategory(e.target.value)} >
+          <Input  type="select" value={category} onChange={(e) => handle(e.target.value)} >
           <option value="Cats">CAT</option>
           <option value="Dogs">DOG</option>
           </Input>
@@ -54,14 +73,14 @@ const PostPet =() =>{
         <Col lg='4'>
         <FormGroup row className="row-cols-lg-auto g-3 align-items-center">
           <span className='s1' for="Name" sm='1'>Name:</span>   
-          <Input type="text"value={Name} onChange={(e) => setName(e.target.value)} placeholder="Pet’s Name"/>  
+          <Input type="text"value={nickname} onChange={(e) => handle(e.target.value)} placeholder="Pet’s Name"/>  
         </FormGroup>
         </Col>
 
         <Col lg='4'>
         <FormGroup row className="row-cols-lg-auto g-3 align-items-center">
           <span className='s1' for="ID" sm='1'>Pet's ID</span>
-          <Input  type="text" value={ID} required onChange={(e) => setId(e.target.value)} placeholder="--- --- ---" />
+          <Input  type="text" value={ID} required onChange={(e) => handle(e.target.value)} placeholder="--- --- ---" />
         </FormGroup>
         </Col>
         </Row>
@@ -70,7 +89,7 @@ const PostPet =() =>{
         <Col lg='4'>
         <FormGroup row className="row-cols-lg-auto g-3 align-items-center">
           <span className='s1' for="Country" sm='1'>Country:</span>
-          <Input type="select" value={Country} onChange={(e) => setCountry(e.target.value)}>
+          <Input type="select" value={country} onChange={(e) => handle(e.target.value)}>
           <option value="Australia">Australia</option>
           </Input>
 
@@ -80,7 +99,7 @@ const PostPet =() =>{
         <Col lg='4'>
         <FormGroup row className="row-cols-lg-auto g-3 align-items-center">
           <span className='s1' for="City" sm={1}>City:</span>
-          <Input type="select" value={City} onChange={(e) => setCity(e.target.value)}>
+          <Input type="select" value={city} onChange={(e) => handle(e.target.value)}>
           <option value="Sydney">Sydney</option>
           <option value="Melbourne">Melbourne</option>
           <option value="Brisbane">Brisbane</option>
@@ -94,7 +113,7 @@ const PostPet =() =>{
         <Col lg='4'>
         <FormGroup row className="row-cols-lg-auto g-3 align-items-center">
           <span className='s1' for="SEX" sm={1}>SEX:</span> 
-          <Input type="select" value={Gender} onChange={(e) => setGender(e.target.value)} >
+          <Input type="select" value={sex} onChange={(e) => handle(e.target.value)} >
           <option value="Boy">MALE</option>
           <option value="Gril">FEMALE</option>
           </Input>
@@ -107,14 +126,14 @@ const PostPet =() =>{
         <Col lg='4'>
         <FormGroup row className="row-cols-lg-auto g-3 align-items-center">
           <span className='s1' for="Age" sm={1}>Age:</span>
-          <Input type="text" required value={Age} onChange={(e) => setAge(e.target.value)} placeholder="month/year" />
+          <Input type="text" required value={age} onChange={(e) => handle(e.target.value)} placeholder="month/year" />
         </FormGroup>
         </Col>
       
         <Col lg='4'>
         <FormGroup row className="row-cols-lg-auto g-3 align-items-center">
           <span className='s1' for="Color" sm={1}>Color:</span>
-          <Input type="select" value={Color} onChange={(e) => setColor(e.target.value)}>
+          <Input type="select" value={color} onChange={(e) => handle(e.target.value)}>
           <option value="Orange">RED</option>
           <option value="Black">BLUE</option>
           <option value="White">GREEN</option>
@@ -128,7 +147,7 @@ const PostPet =() =>{
         <Col lg='4'>
         <FormGroup row className="row-cols-lg-auto g-3 align-items-center">
           <span className='s1' for="Desex" sm={1}>Desex:</span>
-          <Input type="select" value={Immunization} onChange={(e) => setImmunization(e.target.value)}>
+          <Input type="select" value={immunization} onChange={(e) => handle(e.target.value)}>
           <option value="Yes">Yes</option>
           <option value="No">No</option>
           </Input>
@@ -140,7 +159,7 @@ const PostPet =() =>{
         <Col lg={12}>
         <FormGroup row className='row-cols-lg-auto g-3 align-items-center'>
           <span for="exampleText" sm={1}>Descirption:</span>
-          <Input className='textInput' type="textarea" required value={Descirption} onChange={(e) => setDescirption(e.target.value)}/>
+          <Input className='textInput' type="textarea" required value={detail} onChange={(e) => setDetail(e.target.value)}/>
         </FormGroup>
         </Col>
         </Row>
@@ -157,13 +176,13 @@ const PostPet =() =>{
         </FormGroup>
         </Col>
         </Row>
-        { !isPending &&<Button type="submit" value="Submit" className="addPostTest">Post</Button>}
-        { isPending &&<Button disabled className="addPostTest">Adding...</Button>}
+        <Button type="submit" onClick={submit} className="addPostTest">Post</Button>
+      
         </Form>
 
 
     );
-}
+    }
 
 
 export default PostPet;
