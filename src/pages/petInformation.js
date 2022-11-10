e65f395e9d918e6c50487529021f9814fd8c7ba4
luanyl <lollyluan999@qq.com>
2022-11-10 21:23:06 +1100

    try to display image in pet information

--- src/pages/petInformation.js
+++ src/pages/petInformation.js
@@ -11,6 +11,7 @@ import {getPet} from '../api/pet';
 import { useEffect, useState} from 'react';
 import{useParams} from 'react-router-dom';
 import ApplicationPopup from '../Components/applicationPopup';
+import {getImage} from '../api/image';
 
 
 function PetInfo(){
@@ -55,8 +56,6 @@ function PetInfo(){
     func()
     }, []);
 
-    console.log(pet.imageCollectionDTO)
-
     return (
         <div>
             <Header/>
@@ -64,7 +63,7 @@ function PetInfo(){
             <Container style={bodyStyle}>
                 <Row md={1}>
                 <Col md={7}>
-                    {/* <Row><Image src={`data:image/jpeg;base64,${pet.imageCollectionDTO.imageList[0].image}`}/></Row> */}
+                    <Row><Image src={`data:image/jpeg;base64,${pet.imageCollectionDTO.imageList[0].image}`}/></Row>
                     <br/>
                     {/* <Row md = {3}>
                         <Col md={4}> <Image style={imageStyle} src={catd1}/> </Col>
@@ -76,22 +75,16 @@ function PetInfo(){
                 <Col md={5}>
                     <Card  style={infoStyle}>
                             <Card.Body>
-                                <Card.Title>Name</Card.Title>
+                                <Card.Title>{pet.nickname}</Card.Title>
                             <Card.Text style={textStyle}>
-                                Mose is now about a year old, a typical orange patterned male cat 
-                                with a very nice and friendly personality, and has been wandering 
-                                outside for a while, so he may need a little more generous space 
-                                to keep. He has been de-sexed and is healthy in all aspects, 
-                                but his tail was once a bit internally damaged, but has recovered 
-                                and does not affect his normal life. Hope to find a loving family 
-                                and get along well with other cats.
+                                {pet.detail}
                             </Card.Text>
                             <div style={tagStyle}>
                                 <Tags color = {pet.color} sex = {pet.sex} age = {pet.age} 
                                 character = {pet.character} immunization = {pet.immunization}></Tags>
                             </div> 
                             <div style={tagStyle}>
-                                <BsFillGeoAltFill/> Melbourne,VIC
+                                <BsFillGeoAltFill/> {pet.city},{pet.country}
                             </div>                         
                             <ApplicationPopup id = {pet.id}/>
                             </Card.Body>
