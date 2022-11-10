fb111ddc4f0c656c60fbbb4ddfcc7b34add5a48c
Potato-Gravy <min.zhang.1024@gmail.com>
2022-11-11 00:10:07 +1100

    fix some bugs

--- src/pages/DiffLogin.js
+++ src/pages/DiffLogin.js
@@ -1,42 +1,40 @@
-import React from 'react';
-import {useState} from 'react';
-import { Button,Form,Label} from 'reactstrap';
-import {Link} from "react-router-dom";
+import React from 'react'
+import { useState } from 'react'
+import { Button, Form, Label } from 'reactstrap'
+import { Link } from "react-router-dom"
 
 
-export default function DiffLoginPage() {
-    const [isLogin, setIsLogin] = useState(true);
+export default function DiffLoginPage () {
+    const [isLogin, setIsLogin] = useState(true)
 
-    return(
+    return (
         <div>
-          <DiffLogin></DiffLogin>
+            <DiffLogin></DiffLogin>
         </div>
     )
 }
 
-function DiffLogin(){
-    return( 
-    <div>
-        <Form>
-        <Label className="contianer col-lg-5">
-             <h2 className="disabled" href="#">Login as User</h2>
-        </Label>
+function DiffLogin () {
+    return (
+        <div className='diffLogin'>
+            <Form className='form1'>
+                <Label className="contianer col-lg-5">
+                    <h2 className="disabled" href="#">User Login</h2>
+                </Label>
+                <Link to="/login">
+                    <Button className='btn' outline color="success">Login</Button>{' '}
+                </Link>
+            </Form>
 
-        <Link to="/login">
-            <Button className='btn' outline color="success">Login as User</Button>{' '}
-        </Link>
-        </Form>
-    
-        <Form>
-           <Label className="contianer col-lg-5">
-             <h2 className="disabled" href="#">Login as Admin</h2>
-            </Label>
-    
-            <Link to="/admin/login">
-                <Button className='btn' outline color="success">Login as Admin</Button>{' '}
-            </Link>
-        </Form>
-    </div>
+            <Form className='form2'>
+                <Label className="contianer col-lg-5">
+                    <h2 className="disabled" href="#">Admin Login</h2>
+                </Label>
+                <Link to="/admin/login">
+                    <Button className='btn' outline color="success">Login</Button>{' '}
+                </Link>
+            </Form>
+        </div>
     )
 }
 
--- src/pages/Donation/donation.js
+++ src/pages/Donation/donation.js
@@ -1,26 +1,41 @@
-import React from "react";
-import Footer from "../../Components/footer";
-import Header from "../../Components/header";
-import { useState } from "react";
+import React from "react"
+import Footer from "../../Components/footer"
+import Header from "../../Components/header"
+import { useState } from "react"
+import donationImage from "../../images/donation-project1.jpg"
+import './donation.css'
 
-function Donation() {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [amount, setAmount] = useState('');
-    const [expiry, setExpiry] = useState('');
-    const [cvv, setCvv] = useState('');
+function Donation () {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [amount, setAmount] = useState('')
+    const [expiry, setExpiry] = useState('')
+    const [cvv, setCvv] = useState('')
     return (
-        <form>
-            <div>
-                <label>First Name:</label>
+        <div className="donation">
+
+            <h2>Infectious Diseases of a Kitten</h2>
+
+            <div className="article">
+                <img src={donationImage} alt="" />
+                <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
+            </div>
+            <h2>Donation Details</h2>
+            <form className="donationForm">
+                <div>
+                    <label>First Name:</label>
+
                     <input
                         type="text"
                         placeholder='First Name'
                         value={firstName}
                         onChange={(e) => setFirstName(e.target.value)}
                         required />
+                </div>
+
+                <div>
                     <label>Last Name:</label>
                     <input
                         type="text"
@@ -28,77 +43,44 @@ function Donation() {
                         value={lastName}
                         onChange={(e) => setLastName(e.target.value)}
                         required />
-            </div>
-            <div>
-                <label>Email Address:</label>
+                </div>
+
+                <div>
+                    <label>Email Address:</label>
                     <input
                         type="text"
                         placeholder='Example@gmail.com'
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         required />
-            </div>
-            <div>
-                <label>Phone:</label>
-                <input
+                </div>
+
+                <div>
+                    <label>Phone:</label>
+                    <input
                         type="text"
                         placeholder='Phone Number'
                         value={phone}
                         onChange={(e) => setPhone(e.target.value)}
-                        required  />
-            </div>
-            <div>
-                <label>Donation Amount:</label>
-                <input 
-                    type="text"
-                    placeholder="Enter an Amount"
-                    value={amount}
-                    onChange={(e) => setAmount(e.target.value)} />
-            </div>
-            <div>
-                <div>
-                    <label>Payment Methods:</label>
-                    <input 
-                        type="radio"
-                        name = "payment method" />
-                        <label>Debit or Credit Card</label>
-                    <input 
-                        type="radio"
-                        name = "payment method" />
-                    <label>Bank Transfer</label>
-                    <input 
-                        type="radio"
-                        name = "payment method" />
-                    <label>Paypal</label>
-                </div>
-                <div>
-                    <input 
-                        type="tel" 
-                        inputmode="numeric" 
-                        pattern="[0-9\s]{16,16}" 
-                        maxlength="16" placeholder="xxxx xxxx xxxx xxxx"
                         required />
                 </div>
+
                 <div>
-                    <input 
+                    <label>Donation Amount:</label>
+                    <input
                         type="text"
-                        placeholder="mm/yy"
-                        value={expiry}
-                        onChange={(e) => setExpiry(e.target.value)}
-                        required  />
-                    <input 
-                        type="text"
-                        placeholder="CVV"
-                        value={cvv}
-                        onChange={(e) => setCvv(e.target.value)}
-                        required />
+                        placeholder="Enter an Amount"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)} />
                 </div>
-            </div>
-            <div>
-                <button>Proceed</button>
-            </div>
-        </form>
+
+                <div className="">
+                    <button>Proceed</button>
+                </div>
+
+            </form>
+        </div>
     )
 
 }
-export default Donation;
+export default Donation
